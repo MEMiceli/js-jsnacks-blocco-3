c1c3b79e437c6da9955307d18b2106d40aee922b
MEMiceli <marcello.emilio.miceli@gmail.com>
2022-05-05 12:17:49 +0200

    first commit

---
+++ snack-1/main.js
@@ -0,0 +1,16 @@
+// Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
+
+// crea lista vuota
+
+// chiedi inserire dati dentro lista vuota
+
+// ripeti richiesta  fintanto che la somma degli elementi é minore di 50
+
+
+
+
+
+
+
+
+
