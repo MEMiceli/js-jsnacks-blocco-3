bc3e644e1443ecbc4b6f1a7bb8a8b35999a67fde
MEMiceli <marcello.emilio.miceli@gmail.com>
2022-05-05 15:44:48 +0200

    credo corretto

--- snack-2/main.js
+++ snack-2/main.js
@@ -1,2 +1,22 @@
 // Fai inserire un numero, che chiameremo N, all’utente.
 // Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100. Ogni volta che ne crei uno, stampalo a schermo.
+
+const N = Number(prompt("inserire un numero"))
+
+
+// genera numero di array 
+for (let i = 0; i < N; i++){
+    let array =[]
+    for (let i = 0; i < 10; i++){
+        // genera 10 numeri randomici tra 1 e 100
+      let add =  Math.floor(Math.random() * 100)+ 1;
+      array.push(add)
+        // stampalo
+        console.log(array)
+    }
+    
+    
+}
+
+
+
